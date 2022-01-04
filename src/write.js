137fd3256ade2a9b673bb925ae15ad3350585f21
brianiiix <brian890309@gmail.com>
2022-01-04 23:13:06 +0800

    elaborate index.html

--- src/write.js
+++ src/write.js
@@ -3,5 +3,5 @@ document.getElementById("write").addEventListener("click", write);
 function write() {
     alert(tinyMCE.activeEditor.getContent());
     var myContent = tinymce.activeEditor.getContent({ format: 'text' });
-    if(mycontent) console.log(myContent);
+    if(myContent) console.log(myContent);
 }

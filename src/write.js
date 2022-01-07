877e2c2d9094af061d43e73a2c1be74f583b2774
brianiiix <brian890309@gmail.com>
2022-01-07 22:19:20 +0800

    add firebase, you can write and get article from write.html

--- src/write.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("write").addEventListener("click", write);
-
-function write() {
-    var myContent = tinyMCE.activeEditor.getContent();
-    if(myContent) console.log(myContent);
-}
